b7304b7003f3a9ae79f5de2ae83289e3a4b58e09
Jo Vincent Abijero <jovincentabijero31@gmail.com>
2025-08-17 06:38:54 -0700

    deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const dark_mode = document.getElementById('dark-mode')
+const mode_icon = document.getElementById('mode-icon');
+
+dark_mode.addEventListener('click', function () {
+    document.body.classList.toggle('theme-color')
+
+    // Toggle the icon
+    if (document.body.classList.contains('theme-color')) {
+        mode_icon.classList.remove('fa-moon');
+        mode_icon.classList.add('fa-sun');
+    } else {
+        mode_icon.classList.remove('fa-sun');
+        mode_icon.classList.add('fa-moon');
+    }
+
+})
+
+
+
